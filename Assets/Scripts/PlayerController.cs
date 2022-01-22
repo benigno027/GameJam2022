46a8aab4585da59fc80523880094f01e6522587a
benigno027 <benigno027@gmail.com>
2022-01-21 23:02:31 -0500

    se agrego Ferr2D

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //Variables del movimiento del personaje
+    public float jumpForce = 6f;
+    public float runningSpeed = 2f;
+
+    Rigidbody2D rigidBody;
+    Animator animator;
+
+    const string STATE_ALIVE = "isAlive";
+    const string STATE_ON_THE_GROUND = "isOnTheGround";
+
+    public LayerMask groundMask;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        // animator = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        // animator.SetBool(STATE_ALIVE, true);
+        // animator.SetBool(STATE_ON_THE_GROUND, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            Jump();
+        }
+
+        // animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
+
+        Debug.DrawRay(this.transform.position, Vector2.down * 1.5f, Color.red);
+	}
+
+    void FixedUpdate()
+    {
+        if(rigidBody.velocity.x < runningSpeed){
+            rigidBody.velocity = new Vector2(runningSpeed, //x
+                                             rigidBody.velocity.y //y
+                                            );
+        }
+    }
+
+    void Jump()
+    {
+        if (IsTouchingTheGround())
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    //Nos indica si el personaje está o no tocando el suelo
+    bool IsTouchingTheGround(){
+        if(Physics2D.Raycast(this.transform.position,
+                             Vector2.down,
+                             1.5f, 
+                             groundMask)){
+            //TODO: programar lógica de contacto con el suelo
+            return true;
+        }else {
+            //TODO: programar lógica de no contacto
+            return false;
+        }
+    }
+
+}
